147dea52cfccaf229e4db6f2b1e8fd01256714d2
Niklas Larsson <niklas@mm.st>
2020-11-29 14:50:32 +0100

    Add a function to read all lines

---
+++ utils/Library.fs
@@ -0,0 +1,10 @@
+﻿namespace utils
+
+open System
+open System.IO
+
+module IO =
+    let ReadAllLines file =
+        use f = File.OpenText file
+        let content = f.ReadToEnd()
+        content.Split('\n', StringSplitOptions.RemoveEmptyEntries)
